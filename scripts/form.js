dc1d792b66c663c7053d3d4825533b8ad9f082ab
Joshua Garcia <garciajoshua907@yahoo.com>
2021-07-12 12:46:52 -0600

    logic for the form added styles and options clickable buttons to remove and add form are also added

--- scripts/form.js
+++ scripts/form.js
@@ -1,39 +1,34 @@
 const form = document.getElementById("form");
+const mainform = document.getElementById("main-form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
+const formButtons = document.querySelectorAll(".form-subscribe");
 
 function showError(input, message) {
   const formControl = $(input).parent()[0];
-  console.log(formControl);
   $(formControl).addClass("form-control error");
-  const small = $(formControl).find("small");
-  small.innerText = message;
+  const small = $(formControl).find("small")[0];
+  $(small).text(message);
 }
+
 function showSuccess(input) {
-  const formControl = input.parentElement;
-  formControl.className = "form-control success";
+  const formControl = $(input).parent()[0];
+  $(formControl).removeClass("error");
+  $(formControl).addClass("form-control success");
 }
 
 function isValidEmail(email) {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  console.log(re.test(String(email.value).toLowerCase()));
-  re.test(String(email.value).toLowerCase())
-    ? console.log(getFieldName(email) + "working on email")
-    : console.log("other");
+  if (!re.test(String(email.value).toLowerCase())) {
+    showError(email, "invalid email");
+  } else {
+    showSuccess(email);
+  }
 }
 
-function checkRequired(inputArr) {
-  inputArr.forEach(function (input) {
-    if (input.value.trim() === "") {
-      showError(input, `${getFieldName(input)} is required`);
-    } else {
-      showSuccess(input);
-    }
-  });
-}
 function checkInputLegth(input, min, max) {
   if (input.value.length < min) {
     showError(input, `${getFieldName(input)} is less than ${min} characters`);
@@ -44,8 +39,12 @@ function checkInputLegth(input, min, max) {
   }
 }
 function passwordMatchCheck(input1, input2) {
-  if (input1.value !== input2.value) {
+  if (input2.value === "") {
+    showError(input2, "please enter password");
+  } else if (input1.value !== input2.value) {
     showError(input2, `Password dosn't match`);
+  } else {
+    showSuccess(input2);
   }
 }
 
@@ -55,9 +54,15 @@ function getFieldName(input) {
 //Event listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  checkRequired([username, email, password, password2]);
+  isValidEmail(email);
   checkInputLegth(username, 4, 15);
   checkInputLegth(password, 4, 20);
-  isValidEmail(email);
   passwordMatchCheck(password, password2);
 });
+
+for (let i = 0; i < formButtons.length; i++) {
+  console.log(formButtons[i]);
+  formButtons[i].addEventListener("click", () => {
+    mainform.classList.toggle("reveal");
+  });
+}
