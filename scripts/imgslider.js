215e213e21b43e290b0f680446a056bc996957c9
Joshua Garcia <garciajoshua907@yahoo.com>
2021-07-22 17:01:15 -0600

    styles and themes added along with submit form placement.

--- scripts/imgslider.js
+++ scripts/imgslider.js
@@ -1,3 +1,6 @@
 $(document).ready(() => {
   $(".bxslider").bxSlider({ mode: "fade", auto: "true", captions: "true" });
+  $(".bx-wrapper .bx-controls-direction a").css({
+    zIndex: 20,
+  });
 });
